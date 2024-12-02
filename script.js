b472d510ea21e356af4ce64a9fcc53dc19ec3f86
Nastia <nastiaaaa3467@gmail.com>
2024-12-02 21:03:45 +0100

    if \  && \ || tasks

--- script.js
+++ script.js
@@ -1,34 +1,51 @@
 "use strict"
 
-// if (4) {
-//     console.log('Ok!');
-// } else {
-//     console.log('error');
-// }
+console.log( NaN || 2 || undefined );
+//2
+console.log( NaN && 2 && undefined );
+//nan
+console.log( 1 && 2 && 3 );
+//3
+console.log( !1 && 2 || !3 );
+//false
+console.log( 25 || null && !3 );
+//25
+console.log( NaN || null || !3 || undefined || 5);
+//5
+console.log( NaN || null && !3 && undefined || 5);
+//5
+console.log( 5 === 5 && 3 > 1 || 5 );
+//true
 
-// if ( num < 49) {
-//     console.log('error');
-// } else if ( num > 100 ) {
-//     console.log('many')
-// } else {
-//     console.log('ok');
-// }
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
 
-// (num == 50) ? console.log('ok!') : console.log('error');
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {
+//     console.log('Done!')
+// };
+//true
+//Done!
 
-const num = 100;
+// let hamburger;
+//const - can't to have undefiend(condition)
+// const fries = NaN,
+//       cola = 0,
+//       nuggets = 2;
 
-switch (num) {
-    case 49: 
-        console.log('wrong');
-        break;
-    case 100: 
-        console.log('many');
-        break;
-    case 50: 
-        console.log('right');
-        break;
-    default:
-        console.log('not today');
-        break;
+// if (hamburger || cola || fries === 3 || nuggets) {
+//     console.log('Done!')
+// };
+//true
+//2
+
+let hamburger;
+const fries = NaN,
+      cola = 0,
+      nuggets = 2;
+
+if (hamburger && cola || fries === 3 && nuggets) {
+    console.log('Done!')
 };
+//no result
