27328233c37dd754991c6b711a0e510c74192889
Nastia <nastiaaaa3467@gmail.com>
2024-12-02 17:57:20 +0100

    sipmle tasks

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-let select = document.querySelector("select"),
-    html = document.querySelector("html");
+"use strict"
 
-document.body.style.padding = "10px";
+// if (4) {
+//     console.log('Ok!');
+// } else {
+//     console.log('error');
+// }
 
-function update(bgColor, textColor) {
-    html.style.backgroundColor = bgColor;
-    html.style.color = textColor;
-}
+// if ( num < 49) {
+//     console.log('error');
+// } else if ( num > 100 ) {
+//     console.log('many')
+// } else {
+//     console.log('ok');
+// }
 
-select.onchange = function () {
-    select.value === "black"
-        ? update("black", "white")
-        : update("white", "black");
+// (num == 50) ? console.log('ok!') : console.log('error');
+
+const num = 100;
+
+switch (num) {
+    case 49: 
+        console.log('wrong');
+        break;
+    case 100: 
+        console.log('many');
+        break;
+    case 50: 
+        console.log('right');
+        break;
+    default:
+        console.log('not today');
+        break;
 };
