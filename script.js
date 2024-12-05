1f5d4d0ae5dcef17df81c7a2b38b09d2849f9000
Nastia <nastiaaaa3467@gmail.com>
2024-12-05 19:35:36 +0100

    conditions of the cycle | tasks | stars

--- script.js
+++ script.js
@@ -1,83 +1,47 @@
 "use strict"
 
-// function firstTask() {
-//     const arr = [3, 5, 8, 16, 20, 23, 50];
-//     const result = [];
+// *
+// **
+// ***
+// ****
+// *****
+// ******
 
-//     for (let i = 0; i < arr.length; i++) {
-//         result[i] = arr[i];  
+// let result = '';
+// const length = 10;
+
+// for (let i = 1; i < length; i++) {
+
+//     for (let j = 0; j < i; j++) {
+//         result += '*';
 //     }
-//     console.log(result);
-//     return result;
+//     result += '\n';
 // }
+// console.log(result);
 
-firstTask();
 
+//      * 
+//     ***
+//    *****
+//   *******
+//  *********
+// ***********
 
-function firstTask() {
-    const arr = [3, 5, 8, 16, 20, 23, 50];
-    const result = [];
-    let i = 0;
+const lines = 5;
+let result = '';
 
-    while (i < arr.length) {
-        result.push(arr[i]); 
-        i++; 
+for (let i = 0; i <= lines; i++) {
+    for (let j = 0; j < lines - i; j++) {
+        result += " ";
     }
-    console.log(result);
-    return result;
+    for (let j = 0; j < 2 * i + 1; j++) {
+        result += "*";
+    }
+    result += "\n";
 }
 
-firstTask();
+console.log(result)
 
-///////////////////////////////////////////////////////////////////////
-
-// const data = [5, 10, 'Shopping', 20, 'Homework'];
-// let i = 0;
-//     while (i < data.length) {
-//         if (typeof(data[i]) === 'number') {
-//             data[i] = data[i] * 2;  
-//         } else if (typeof(data[i]) === 'string') {
-//             data[i] = `${data[i]} - done`;
-//         }
-//         i++;
-//     }
-// console.log(data);
-// return data;
-
-//[ 10, 20, 'Shopping - done', 40, 'Homework - done' ]
-
-// function secondTask() {
-//     const data = [5, 10, 'Shopping', 20, 'Homework'];
-
-//     for (let i = 0; i < data.length; i++) {
-//         if (typeof(data[i]) === 'number') {
-//             data[i] += data[i];
-//         } else if (typeof(data[i]) === 'string') {
-//             data[i] = `${data[i]} - done`;
-//         }
-//     }
-//     console.log(data);
-//     return data;
-    
-// }
-
-// secondTask();
-
-////////////////////////////////////////////////////////////////////////
-
-function thirdTask() {
-    const data = [5, 10, 'Shopping', 20, 'Homework'];
-    const result = [];
-        for (let i = 0; i < data.length; i++) {
-            result[i - 1] = data[data.length - i]  
-        }
-    console.log(result);
-    return result;
+function numberToString(num) {
+    return num.toString();
 }
-
-thirdTask();
-
-// result[i - 1] = data[data.length - i];
-// data.length - i дає індекс елемента в масиві data, починаючи з останнього елемента (наприклад, для i = 1 це буде data[4], для i = 2 — data[3] і так далі).
-// result[i - 1] дозволяє записати елементи в масив result з індексами від 0 до 4 (так як індексація масивів в JavaScript починається з 0).
-// Таким чином, ми копіюємо елементи з масиву data в масив result у зворотному порядку.
