6e1bc7c05d5133e41fe6d85661e5ac47cb346cef
Nastia <nastiaaaa3467@gmail.com>
2024-12-05 18:27:47 +0100

    conditions of the cycle | tasks | advanced

--- script.js
+++ script.js
@@ -1,26 +1,83 @@
 "use strict"
 
-function fifthTask() {
-    const arrayOfNumbers = [];  
+// function firstTask() {
+//     const arr = [3, 5, 8, 16, 20, 23, 50];
+//     const result = [];
 
-    for (let i = 5; i < 11; i++) {  
-        arrayOfNumbers[i - 5] = i;  
+//     for (let i = 0; i < arr.length; i++) {
+//         result[i] = arr[i];  
+//     }
+//     console.log(result);
+//     return result;
+// }
+
+firstTask();
+
+
+function firstTask() {
+    const arr = [3, 5, 8, 16, 20, 23, 50];
+    const result = [];
+    let i = 0;
+
+    while (i < arr.length) {
+        result.push(arr[i]); 
+        i++; 
     }
-
-    console.log(arrayOfNumbers); 
-    return arrayOfNumbers; 
+    console.log(result);
+    return result;
 }
 
-fifthTask();  // 6
+firstTask();
 
+///////////////////////////////////////////////////////////////////////
 
-// arrayOfNumbers[i - 5] = i;
-// В кожній ітерації циклу ми записуємо значення i в масив за індексом i - 5. Це дозволяє нам почати з індексу 0 і зберегти значення чисел у масиві:
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+// let i = 0;
+//     while (i < data.length) {
+//         if (typeof(data[i]) === 'number') {
+//             data[i] = data[i] * 2;  
+//         } else if (typeof(data[i]) === 'string') {
+//             data[i] = `${data[i]} - done`;
+//         }
+//         i++;
+//     }
+// console.log(data);
+// return data;
 
-// Коли i = 5, в масив записується значення 5 за індексом 5 - 5 = 0.
-// Коли i = 6, в масив записується значення 6 за індексом 6 - 5 = 1.
-// І так далі, до i = 10, де значення 10 записується за індексом 10 - 5 = 5.
-// Таким чином, у масиві з'являться числа: [5, 6, 7, 8, 9, 10].
-// Повернення масиву:
-// return arrayOfNumbers;
-// Функція повертає масив arrayOfNumbers, щоб його можна було використовувати поза функцією, якщо потрібно.
+//[ 10, 20, 'Shopping - done', 40, 'Homework - done' ]
+
+// function secondTask() {
+//     const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+//     for (let i = 0; i < data.length; i++) {
+//         if (typeof(data[i]) === 'number') {
+//             data[i] += data[i];
+//         } else if (typeof(data[i]) === 'string') {
+//             data[i] = `${data[i]} - done`;
+//         }
+//     }
+//     console.log(data);
+//     return data;
+    
+// }
+
+// secondTask();
+
+////////////////////////////////////////////////////////////////////////
+
+function thirdTask() {
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    const result = [];
+        for (let i = 0; i < data.length; i++) {
+            result[i - 1] = data[data.length - i]  
+        }
+    console.log(result);
+    return result;
+}
+
+thirdTask();
+
+// result[i - 1] = data[data.length - i];
+// data.length - i дає індекс елемента в масиві data, починаючи з останнього елемента (наприклад, для i = 1 це буде data[4], для i = 2 — data[3] і так далі).
+// result[i - 1] дозволяє записати елементи в масив result з індексами від 0 до 4 (так як індексація масивів в JavaScript починається з 0).
+// Таким чином, ми копіюємо елементи з масиву data в масив result у зворотному порядку.
