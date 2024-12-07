cd2e85cffc3a0eb77239b1181b1cc301a2934c17
Nastia <nastiaaaa3467@gmail.com>
2024-12-07 11:34:42 +0100

    practice, part 2. Applying conditions and cycles

--- script.js
+++ script.js
@@ -1,47 +1,16 @@
 "use strict"
+/* Assignment for the lesson:
 
-// *
-// **
-// ***
-// ****
-// *****
-// ******
+1) Automate questions to the user about movies using a loop
 
-// let result = '';
-// const length = 10;
+2) Make it so that the user cannot leave an answer as an empty string,
+cancel the answer or enter a movie title longer than 50 characters. If this happens,
+return the user to the questions again. (You can refer to any string as
+str.length - and get its length)
 
-// for (let i = 1; i < length; i++) {
+3) Using conditions, check personalMovieDB.count, and if it is less than 10 - display the message
+"Very few movies watched", if from 10 to 30 - "You are a classic lover", and if more -
+"You are a movie lover". And if none of the options are suitable - "An error occurred"
 
-//     for (let j = 0; j < i; j++) {
-//         result += '*';
-//     }
-//     result += '\n';
-// }
-// console.log(result);
+4) Practice and rewrite the loop in two more ways*/
 
-
-//      * 
-//     ***
-//    *****
-//   *******
-//  *********
-// ***********
-
-const lines = 5;
-let result = '';
-
-for (let i = 0; i <= lines; i++) {
-    for (let j = 0; j < lines - i; j++) {
-        result += " ";
-    }
-    for (let j = 0; j < 2 * i + 1; j++) {
-        result += "*";
-    }
-    result += "\n";
-}
-
-console.log(result)
-
-function numberToString(num) {
-    return num.toString();
-}
